59a34ffa068ca28b9424194dd92291ff7dd159ea
Dirk Kröhan <dirk.kroehan@qaware.de>
2023-06-23 17:30:08 +0200

    SDK man installation + Tilt

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,6 +8,17 @@ echo "$(date)    post-create start" >> "$HOME/status"
 # update the repos
 #git -C /workspaces/imdb-app pull
 #git -C /workspaces/webvalidate pull
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+echo "Installed SDK MAN"
+
+sdk install java 17.0.7-zulu
+
+echo "Installed Java 17.0.7-zulu"
+
+curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
+
+echo"Installed Tilt"
 
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
