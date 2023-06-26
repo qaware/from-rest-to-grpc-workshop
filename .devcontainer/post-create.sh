e5ca8ecdd8dcf2b3415305476e41bcb7f9a1197a
Dirk Kröhan <dirk.kroehan@qaware.de>
2023-06-26 12:42:35 +0200

    Fix go installation

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -42,7 +42,7 @@ echo "Installing Golang"
 
 wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
 
