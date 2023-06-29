176ca15ccfd0f6e51d37ee8251b652e43c25a30e
Dirk Kröhan <dirk.kroehan@qaware.de>
2023-06-29 08:28:16 +0200

    Cleanup

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,14 +5,6 @@
 echo "on-create start"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create start" >> "$HOME/status"
 
-# clone repos
-#git clone https://github.com/cse-labs/imdb-app /workspaces/imdb-app
-#git clone https://github.com/microsoft/webvalidate /workspaces/webvalidate
-
-# restore the repos
-#dotnet restore /workspaces/webvalidate/src/webvalidate.sln
-#dotnet restore /workspaces/imdb-app/src/imdb.csproj
-
 export REPO_BASE=$PWD
 export PATH="$PATH:$REPO_BASE/cli"
 
@@ -31,36 +23,12 @@ docker network create k3d
 k3d registry create registry.localhost --port 5500
 docker network connect k3d k3d-registry.localhost
 
-# update the base docker images
-#docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-#docker pull mcr.microsoft.com/dotnet/sdk:6.0
-#docker pull ghcr.io/cse-labs/webv-red:latest
-
-# echo "dowloading kic CLI"
-# cd cli || exit
-# tag="0.4.3"
-# wget -O kic.tar.gz "https://github.com/retaildevcrews/akdc/releases/download/$tag/kic-$tag-linux-amd64.tar.gz"
-# tar -xvzf kic.tar.gz
-# rm kic.tar.gz
-# cd "$OLDPWD" || exit
-
 echo "generating completions"
-#kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
+kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
 kubectl completion zsh > "$HOME/.oh-my-zsh/completions/_kubectl"
 
 echo "creating k3d cluster"
 kic cluster create
-#kic cluster rebuild
-
-#echo "bilding IMDb"
-#kic build imdb
-
-#echo "building WebValidate"
-#sed -i "s/RUN dotnet test//g" /workspaces/webvalidate/Dockerfile
-#kic build webv
-
-#echo "deploying k3d cluster"
-#kic cluster deploy
 
 # only run apt upgrade on pre-build
 if [ "$CODESPACE_NAME" = "null" ]
